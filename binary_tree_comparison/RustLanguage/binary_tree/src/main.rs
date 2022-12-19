ab6bd660cacecf316e9033b7054b59795d5cdf8e
Francesc <iovinemeccanica@gmail.com>
2022-12-19 23:41:59 +0100

    Created node stuct

--- binary_tree_comparison/RustLanguage/binary_tree/src/main.rs
+++ binary_tree_comparison/RustLanguage/binary_tree/src/main.rs
@@ -1,4 +1,10 @@
 mod node;
+
 fn main() {
-    println!("Hello, world!");
+    let mut root = node::Node::new("1");
+    root.set_left(node::Node::new("1.1"));
+/*    root.left.left = node::Node::new("1.1.1");
+    root.right = node::Node::new("1.2");
+    root.right.left = node::Node::new("1.2.1");
+    */
 }
--- binary_tree_comparison/RustLanguage/binary_tree/src/node.rs
+++ binary_tree_comparison/RustLanguage/binary_tree/src/node.rs
@@ -1,10 +1,32 @@
-enum NodeRef {
-    Some(&mut Node),
-    None,
+// guardare https://dawchihliou.github.io/articles/binary-tree-insertion-in-rust
+pub enum NodeRef {
+    Some(Box<Node>),
+    None
 }
 
-struct Node {
-    payload : String,
-    left : NodeRef,
-    right : NodeRef,
+pub struct Node {
+    pub payload : String,
+    pub left : NodeRef,
+    pub right : NodeRef,
+}
+
+impl Node {
+    pub fn new(s : &str) -> Node {
+        let result = Node {
+            payload : String::from(s),
+            left : NodeRef::None,
+            right : NodeRef::None,
+        };
+        
+        result
+    }
+
+    pub fn set_left(&self, node : &mut Node) {
+        self.left = Box::<node>;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
 }
